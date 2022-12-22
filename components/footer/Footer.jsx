4e4d769dfcec7bb3e2c7504db88cc4f8c4de4d6c
Ol4b0d3 <no.pl4c3.l1k3.127.0.0.1@gmail.com>
2022-12-21 22:31:57 -0600

    footer styling change

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
         <About />
         <Nav />
       </div>
-      <div className="bg-red-600 h-14 xl:justify-between justify-center text-center            md:flex items-center px-10">
+      <div className="bg-red-600 xl:justify-between justify-center text-center font-bold md:flex items-center px-10 p-3">
         <p className="text-white">
           Copyrigth <span className="text-[#ff9300]">slot.ng</span> 2022 All
           Rights Reserverd.
